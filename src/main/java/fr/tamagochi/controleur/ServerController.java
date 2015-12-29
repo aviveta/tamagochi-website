0d0af56edf009c9028c369595786590541369ce7
sakurachiru <yvette.cheng@laposte.net>
2015-12-29 22:42:29 +0100

    update ServerController

--- src/main/java/fr/tamagochi/controleur/ServerController.java
+++ src/main/java/fr/tamagochi/controleur/ServerController.java
@@ -2,22 +2,25 @@ package controleur;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/controleur")
 public class ServerController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
 
 	public ServerController() {
 		super();
 	}
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("../../../../../../mockups/index2.html");
 	}
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 }
