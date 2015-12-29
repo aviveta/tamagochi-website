13d830f0bbef2d8214e2f30a4a93da3c43590232
sakurachiru <yvette.cheng@laposte.net>
2015-12-29 21:48:34 +0100

    add controleur

---
+++ src/main/java/fr/tamagochi/controleur/ServerController.java
@@ -0,0 +1,23 @@
+package controleur;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ServerController extends HttpServlet {
+
+	public ServerController() {
+		super();
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("../../../../../../mockups/index2.html");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
