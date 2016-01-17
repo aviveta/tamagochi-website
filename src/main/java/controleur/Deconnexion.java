f453bfe5ade2a0e7ae9ff4f7cf2c2d213a0fc486
sakurachiru <yvette.cheng@laposte.net>
2016-01-18 00:23:00 +0100

    add logout

---
+++ src/main/java/controleur/Deconnexion.java
@@ -0,0 +1,27 @@
+package controleur;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+ @WebServlet("/logout")
+public class Deconnexion extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public Deconnexion() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      request.getSession().invalidate();
+      request.getRequestDispatcher("jsp/index.jsp").forward(request, response);
+  }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+}
